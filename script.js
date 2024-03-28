c6f37309ddf33a3096d8fd0a905e2454c99b7a8f
M.can <caglayan.mertcan37@gmail.com>
2024-03-28 21:37:37 +0300

    animations added

--- script.js
+++ script.js
@@ -1,10 +1,47 @@
-const nextBtn = document.querySelector("#nextBtn")
-const prevBtn = document.querySelector("#prevBtn")
-const carousel = document.querySelector(".carousel")
+const nextBtn = document.querySelector("#nextBtn");
+const prevBtn = document.querySelector("#prevBtn");
+const carousel = document.querySelector(".carousel");
+const images = document.querySelectorAll(".imgContainer");
+const contents = document.querySelectorAll(".mainContent");
 
-nextBtn.addEventListener("click", ()=> {
-    carousel.style.transform = "translateY(-200%)"
-})
-prevBtn.addEventListener("click", ()=> {
-    carousel.style.transform = "translateY(0%)"
-})
+nextBtn.addEventListener("click", () => {
+	carousel.style.transform = "translateY(-200%)";
+});
+prevBtn.addEventListener("click", () => {
+	carousel.style.transform = "translateY(0%)";
+});
+
+let isAnimating = false;
+
+function showContent(imageId) {
+	isAnimating = true;
+
+	contents.forEach((content) => {
+		if (content.id === imageId && content.classList.contains("active")) {
+			return;
+		}
+
+		if (content.id === imageId) {
+			content.classList.add("active");
+			content.classList.remove("deactive");
+		} else if (content.classList.contains("active")) {
+			content.classList.add("deactive");
+			setTimeout(() => {
+				content.classList.remove("active");
+				content.classList.remove("deactive");
+			}, 500);
+		}
+		setTimeout(() => {
+			isAnimating = false;
+		}, 500);
+	});
+}
+
+images.forEach((image) => {
+	image.addEventListener("click", () => {
+		if (!isAnimating) {
+			const imageId = image.getAttribute("for");
+			showContent(imageId);
+		}
+	});
+});
